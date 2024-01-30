c0d926bcf4832bf329c34f28ad438ed8e02e460f
Gideon Faith Omorodion <grail.masterpiece@gmail.com>
2024-01-30 09:08:31 +0100

    added create-new-project and user-recent-issues resolvers to graph (#8)

--- src/graphql/resolvers/resolve.map.ts
+++ src/graphql/resolvers/resolve.map.ts
@@ -1,21 +1,27 @@
 import GraphQLJSON from "graphql-type-json";
 import { DateScalar } from "../date.scaler.js";
 import { commentResolver } from "./comment.resolve.js";
-import { IssueResolver } from "./issue.resolve.js";
-import { projectResolver } from "./project.resolve.js";
-import { updateResolver } from "./update.resolve.js";
+import { IssueResolver } from "./issue.js";
+import { projectResolver } from "./project.js";
+import { updateResolver } from "./update.js";
 import { resolveProjects } from "./user.projects.js";
-import { userResolver } from "./user.resolve.js";
+import { userResolver } from "./user.js";
 import { resolveProjectsCount } from "./project.count.js";
 import { resolveCreateProject } from "./create.project.js";
+import searchProjectByName from "./search.project.by.name.js";
+import { userRecentIssue } from "./recentIssue.js";
 
 export const resolvers = {
 	Query: {
 		projects: resolveProjects,
 		projectsCount: resolveProjectsCount,
+		searchProjectByName: searchProjectByName,
+		userRecentIssues: userRecentIssue,
 	},
 
-	Mutation: { createProject: resolveCreateProject },
+	Mutation: {
+		createProject: resolveCreateProject,
+	},
 
 	User: userResolver,
 	Project: projectResolver,
